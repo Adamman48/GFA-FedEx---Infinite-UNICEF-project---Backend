a319b5ee09ef77e4fa37d6aa4e812b0440f02131
Illés Alexander <illes.alexander88@gmail.com>
2019-05-03 00:29:12 +0200

    merging

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,9 +1,10 @@
 const GetTheQuestions = require('../services/questions-service');
 
 const QuestionController = (req, res) => {
+ 
   GetTheQuestions()
-    .then((result) => res.json(result))
-    .catch((err) => console.error(err))
+    .then((result) =>  res.json(result))
+    .catch((err) => console.info(err))
 }
 
 module.exports = QuestionController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,5 +11,4 @@ router.post('/add', addQuestion);
 router.get('/load', QuestionController);
 
 
-
 module.exports = router;
